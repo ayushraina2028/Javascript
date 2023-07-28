1361266ae211fe9d3234c03212a586d9f56b9dba
Ayush Raina <ayushraina@iisc.ac.in>
2023-07-29 04:39:01 +0530

    basic things

---
+++ JS/main.js
@@ -0,0 +1,19 @@
+const myName = "Ayush Raina"
+
+// Print and find length of myName
+console.log(myName)
+console.log(myName.length)
+console.log("My name is Ayush Raina".length)
+
+// Printing some part Method 1
+console.log(myName[0])
+console.log(myName[1])
+
+// Method 2
+console.log(myName.charAt(0))
+console.log(myName.charAt(1))
+
+// Find index of group of characters
+console.log(myName.indexOf("sh"))
+
+
